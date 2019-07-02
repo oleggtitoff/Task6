62a8d988cc126de2936be000814e2c02babf4d37
oleggtitoff <46402619+oleggtitoff@users.noreply.github.com>
2019-07-02 14:06:53 +0300

    Updated main.c
    Added struct AllpassBuff.
    
    Added functions:
    doubleToFixed31,
    openFile,
    readHeader,
    writeHeader,
    initializeBuff,
    calculateCoeff.

--- Task6/main.c
+++ Task6/main.c
@@ -1,8 +1,116 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+#define INPUT_FILE_NAME "TestSound4.wav"
+#define OUTPUT_FILE_NAME "Output.wav"
+#define FILE_HEADER_SIZE 44
+#define BYTES_PER_SAMPLE 2
+#define DATA_BUFF_SIZE 1000
+#define SAMPLE_RATE 48000
+#define CHANNELS 2
+
+#define FREQUENCY 200
+
+#define PI 3.14159265358979323846
+
+
+typedef struct {
+	int32_t x;
+	int32_t y;
+} AllpassBuff;
+
+
+int32_t doubleToFixed31(double x);
+
+FILE * openFile(char *fileName, _Bool mode);		//if 0 - read, if 1 - write
+void readHeader(uint8_t *headerBuff, FILE *inputFilePtr);
+void writeHeader(uint8_t *headerBuff, FILE *outputFilePtr);
+
+void initializeBuff(AllpassBuff *buff);
+int32_t calculateCoeff(double Fc);
+
+
 int main()
 {
+
 
 	system("pause");
 	return 0;
 }
+
+
+int32_t doubleToFixed31(double x)
+{
+	if (x >= 1)
+	{
+		return INT32_MAX;
+	}
+	else if (x < -1)
+	{
+		return INT32_MIN;
+	}
+
+	return (int32_t)(x * (double)(1LL << 31));
+}
+
+FILE * openFile(char *fileName, _Bool mode)		//if 0 - read, if 1 - write
+{
+	FILE *filePtr;
+
+	if (mode == 0)
+	{
+		if ((filePtr = fopen(fileName, "rb")) == NULL)
+		{
+			printf("Error opening input file\n");
+			system("pause");
+			exit(0);
+		}
+	}
+	else
+	{
+		if ((filePtr = fopen(fileName, "wb")) == NULL)
+		{
+			printf("Error opening output file\n");
+			system("pause");
+			exit(0);
+		}
+	}
+
+	return filePtr;
+}
+
+void readHeader(uint8_t *headerBuff, FILE *inputFilePtr)
+{
+	if (fread(headerBuff, FILE_HEADER_SIZE, 1, inputFilePtr) != 1)
+	{
+		printf("Error reading input file (header)\n");
+		system("pause");
+		exit(0);
+	}
+}
+
+void writeHeader(uint8_t *headerBuff, FILE *outputFilePtr)
+{
+	if (fwrite(headerBuff, FILE_HEADER_SIZE, 1, outputFilePtr) != 1)
+	{
+		printf("Error writing output file (header)\n");
+		system("pause");
+		exit(0);
+	}
+}
+
+void initializeBuff(AllpassBuff *buff)
+{
+	buff->x = 0;
+	buff->y = 0;
+}
+
+int32_t calculateCoeff(double Fc)
+{
+	return doubleToFixed31((tan(PI * Fc / SAMPLE_RATE) - 1) / (tan(PI * Fc / SAMPLE_RATE) + 1));
+}
+
